18e8f5d14c4466fa61f42abf2e443549a30fe99c
DESKTOP-PBREBOD\ILYAS <ihayrat786@gmail.com>
2022-03-29 14:02:12 +0300

    added redirect to google maps

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -4,6 +4,7 @@ import 'package:bloc_earthquake_app/bloc/earthquake_bloc.dart';
 import 'package:bloc_earthquake_app/constants/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -25,35 +26,42 @@ class _HomePageState extends State<HomePage> {
       child: BlocBuilder<EarthquakeBloc, EarthquakeState>(
         builder: (context, state) {
           if (state is EarthquakeLoadingState) {
-            return Center(child: CircularProgressIndicator(color: Colors.red,));
+            return Center(
+                child: CircularProgressIndicator(
+              color: Colors.red,
+            ));
           } else if (state is EarthquakeLoadedState) {
             return ListView.builder(
                 itemCount: state.earthquake!.result!.length,
                 itemBuilder: (context, index) {
-                  return Card(
-                    color: Colors.red[400],
-                    child: ListTile(
-                      leading: Text(
-                        (index + 1).toString(),
-                        style: AppTextStyles.boldAndSizedText,
-                      ),
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            state.earthquake!.result![index].title.toString(),
-                            textAlign: TextAlign.center,
-                            style: AppTextStyles.boldAndSizedText,
-                          ),
-                          Text(
-                            state.earthquake!.result![index].date.toString(),
-                          ),
-                        ],
-                      ),
-                      trailing: Text(
-                        '${state.earthquake!.result![index].mag}',
-                        style: AppTextStyles.boldText,
+                  return InkWell(
+                    onTap: () => launch(
+                        'https://www.google.com/maps/search/?api=1&query=${state.earthquake!.result![index].lat},${state.earthquake!.result![index].lng}'),
+                    child: Card(
+                      color: Colors.red[400],
+                      child: ListTile(
+                        leading: Text(
+                          (index + 1).toString(),
+                          style: AppTextStyles.boldAndSizedText,
+                        ),
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              state.earthquake!.result![index].title.toString(),
+                              textAlign: TextAlign.center,
+                              style: AppTextStyles.boldAndSizedText,
+                            ),
+                            Text(
+                              state.earthquake!.result![index].date.toString(),
+                            ),
+                          ],
+                        ),
+                        trailing: Text(
+                          '${state.earthquake!.result![index].mag}',
+                          style: AppTextStyles.boldText,
+                        ),
                       ),
                     ),
                   );
